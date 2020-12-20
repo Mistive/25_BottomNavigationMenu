17ff2b2bb17275ccb6e662b4b9c18bcfa2d87c6e
pms940725 <pms940725@naver.com>
2020-12-20 23:23:34 +0900

    Modify MainActivity.java  * delete setFragment()

--- app/src/main/java/com/mistive/bottomnavi/MainActivity.java
+++ app/src/main/java/com/mistive/bottomnavi/MainActivity.java
@@ -15,64 +15,38 @@ public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     private FragmentTransaction ft;
-    private fragment1 fragment_1;
-    private fragment2 fragment_2;
-    private fragment3 fragment_3;
+    private fragment1 fg1 = new fragment1();
+    private fragment2 fg2 = new fragment2();
+    private fragment3 fg3 = new fragment3();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                ft = getSupportFragmentManager().beginTransaction();
                 switch(menuItem.getItemId()){
                     case R.id.action_call:
-                        setFragment(0);
-                        Log.e("TEST", "1");
-                        break;
+                        ft.replace(R.id.frame, fg1).commit();
+                        return true;
                     case R.id.action_chat:
-                        setFragment(1);
-                        Log.e("TEST", "2");
-                        break;
+                        ft.replace(R.id.frame, fg2).commit();
+                        return true;
                     case R.id.action_contacts:
-                        setFragment(2);
-                        Log.e("TEST", "3");
-                        break;
+                        ft.replace(R.id.frame, fg3).commit();
+                        return true;
                 }
                 return true;
             }
         });
-
-        fragment_1 = new fragment1();
-        fragment_2 = new fragment2();
-        fragment_3 = new fragment3();
-        setFragment(0); //첫 프래그먼트 지정
-
-    }
-
-//    프래그먼트 교체가 발생하는 실행문
-    private void setFragment(int n) {
         ft = getSupportFragmentManager().beginTransaction();
-        switch(n){
-            case 0:
-                ft.replace(R.id.frame, new fragment1());
-                ft.commit();
-                break;
-
-            case 1:
-                ft.replace(R.id.frame, new fragment2());
-                ft.commit();
-                break;
-
-            case 2:
-                ft.replace(R.id.frame, new fragment3());
-                ft.commit();
-                break;
-        }
+        ft.replace(R.id.frame, fg1).commit();
     }
+
+
 }
